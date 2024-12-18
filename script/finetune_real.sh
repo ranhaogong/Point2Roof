29f986f2f958fcf2e5bf4a24ce0a43750011e627
ranhaogong <ranhaogong@qq.com>
2024-12-18 14:49:02 +0800

    store model pth to best_output

--- script/finetune_real.sh
+++ script/finetune_real.sh
@@ -3,6 +3,6 @@ python ../train.py \
 --data_path /data/haoran/dataset/RoofReconstructionDataset/RealDataset \
 --cfg_file ../cfg/model_cfg.yaml \
 --batch_size 16 \
---extra_tag original_syndata_realdata_finetune5 \
---epochs 110 \
+--extra_tag edgeconv_multiscalefusion_syndata_realdata_finetune2 \
+--epochs 130 \
 --lr 1e-3
