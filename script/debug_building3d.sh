f569b8f9fa894a1ec5fb0212c9fcecf7b3b23292
ranhaogong <ranhaogong@qq.com>
2025-02-19 16:57:32 +0800

    change optimizer and schedular

--- script/debug_building3d.sh
+++ script/debug_building3d.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=2 \
-python -m debugpy --listen 5684 --wait-for-client ../train_building3d.py \
+CUDA_VISIBLE_DEVICES=1 \
+python -m debugpy --listen 5678 --wait-for-client ../train_building3d.py \
 --data_path /data/haoran/dataset/building3d/Point2Roof \
 --cfg_file ../cfg/model_cfg.yaml \
 --batch_size 64 \
---extra_tag debug_7 \
---epochs 90 \
+--extra_tag debug_12 \
+--epochs 1 \
 --lr 1e-3
--- train_building3d.py
+++ train_building3d.py
@@ -11,16 +11,66 @@ from model.roofnet import RoofNet
 from torch import optim
 from utils import common_utils
 from model import model_utils
+import torch.optim.lr_scheduler as lr_scheduler
 
 
-def get_scheduler(optim, last_epoch):
-    scheduler = torch.optim.lr_scheduler.StepLR(optim, 20, 0.5, last_epoch=last_epoch)
+class WarmupScheduler(lr_scheduler._LRScheduler):
+    """
+    Implements a learning rate warm-up schedule followed by a Cosine Annealing schedule.
+    The learning rate will start small and increase linearly for the first `warmup_epochs` epochs.
+    After that, it follows a CosineAnnealingLR schedule.
+    """
+    def __init__(self, optimizer, warmup_epochs, max_epochs, eta_min=1e-6, last_epoch=-1):
+        self.warmup_epochs = warmup_epochs
+        self.max_epochs = max_epochs
+        self.eta_min = eta_min
+        self.base_lr = optimizer.param_groups[0]['lr']
+        super(WarmupScheduler, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        # Get the current epoch
+        epoch = self.last_epoch
+
+        if epoch < self.warmup_epochs:
+            # Linear warmup phase
+            warmup_lr = self.base_lr * (epoch + 1) / self.warmup_epochs
+            return [warmup_lr for _ in self.optimizer.param_groups]
+        else:
+            # Cosine annealing phase
+            cosine_lr = self.eta_min + 0.5 * (self.base_lr - self.eta_min) * \
+                        (1 + torch.cos(torch.tensor(epoch - self.warmup_epochs) * torch.pi / (self.max_epochs - self.warmup_epochs)))
+            return [cosine_lr for _ in self.optimizer.param_groups]
+
+def get_optimizer_and_scheduler(net, args):
+    # AdamW 优化器
+    optimizer = optim.AdamW(net.parameters(), lr=args.lr, weight_decay=1e-4)
+
+    # 设置学习率热身和 Cosine Annealing 调度器
+    scheduler = WarmupScheduler(optimizer, warmup_epochs=8, max_epochs=args.epochs, eta_min=1e-6)
+
+    return optimizer, scheduler
+
+
+def get_scheduler(name, optim, last_epoch):
+    if name == "steplr":
+        scheduler = torch.optim.lr_scheduler.StepLR(optim, 20, 0.5, last_epoch=last_epoch)
+    elif name == "cosine":
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, T_max=20, eta_min=0, last_epoch=last_epoch)
     return scheduler
 
-
+def get_optimizer(name, net, lr):
+    if name == "adam":
+        opt = optim.Adam(net.parameters(), lr=lr, weight_decay=1e-3)
+    elif name == "adamw":
+        opt = optim.AdamW(net.parameters(), lr=lr, weight_decay=1e-3)
+    return opt
+    
 def parse_config():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type=str, default='../GithubDeepRoof', help='dataset path')
+    parser.add_argument('--scheduler', type=str, default='steplr', help='scheduler:steplr, cosine')
+    parser.add_argument('--optimizer', type=str, default='adam', help='optimizer:adam, adamw')
+    parser.add_argument('--max_ckpt_save_num', type=int, default=5, help='max_ckpt_save_num')
     parser.add_argument('--cfg_file', type=str, default='./model_cfg.yaml', help='model config for training')
     parser.add_argument('--batch_size', type=int, default=64, help='batch size for training')
     parser.add_argument('--gpu', type=str, default='1', help='gpu for training')
@@ -29,8 +79,10 @@ def parse_config():
     parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
     args = parser.parse_args()
     cfg = common_utils.cfg_from_yaml_file(args.cfg_file)
-
+    for arg, value in vars(args).items():
+        print(f"{arg}: {value}")
     return args, cfg
+
 
 
 def main():
@@ -54,8 +106,9 @@ def main():
 
     net = RoofNet(cfg.MODEL, color=cfg.COLOR, nir=cfg.NIR, intensity=cfg.INTENSITY)
     net.cuda()
-    optimizer = optim.Adam(net.parameters(), lr=args.lr, weight_decay=1e-3)
-
+    # optimizer = get_optimizer(args.optimizer, net, args.lr)
+    
+    optimizer, scheduler = get_optimizer_and_scheduler(net, args)
     start_epoch = it = 0
     last_epoch = -1
     ckpt_list = glob.glob(str(ckpt_dir / '*checkpoint_epoch_*.pth'))
@@ -67,14 +120,14 @@ def main():
         )
         last_epoch = start_epoch + 1
 
-    scheduler = get_scheduler(optimizer, last_epoch=last_epoch)
+    # scheduler = get_scheduler(args.scheduler, optimizer, last_epoch=last_epoch)
 
     net = net.train()
     logger.info('**********************Start training**********************')
     #logger.info(net)
     
 
-    train_model(net, optimizer, train_loader, scheduler, it, start_epoch, args.epochs, ckpt_dir, logger = logger, log_file = log_file)
+    train_model(net, optimizer, train_loader, scheduler, it, start_epoch, args.epochs, ckpt_dir, logger = logger, log_file = log_file, max_ckpt_save_num=args.max_ckpt_save_num)
 
 
 if __name__ == '__main__':
